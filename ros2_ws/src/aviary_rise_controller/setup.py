c92dc970d028289a3a3fe6f2a1f199c73073c2f7
Max Gardenswartz <max.gardenswartz@proton.me>
2026-08-24 17:35:56 -0400

    chore: bump aviary_rise_controller to 0.3.0

--- ros2_ws/src/aviary_rise_controller/setup.py
+++ ros2_ws/src/aviary_rise_controller/setup.py
@@ -6,7 +6,7 @@ package_name = 'aviary_rise_controller'
 
 setup(
     name=package_name,
-    version='0.2.0',
+    version='0.3.0',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -17,7 +17,7 @@ setup(
     zip_safe=True,
     maintainer='Max Gardenswartz',
     maintainer_email='mgardenswartz@ufl.edu',
-    description='Second-order RISE controller',
+    description='Second-order RISE controller for the Aviary',
     license='Apache-2.0',
     install_requires=[
         'setuptools',
